22d6687357db2998be7b386c7b6283de0dddbfe3
Timmy Martin <timmywmartin@gmail.com>
2014-03-05 23:25:35 -0700

    Intialize respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnapShop::Application.config.secret_key_base = 'ef10ddbe48afaa5d0397143619c67b958e7fc807018d74bc4e8650e410bbcf49e8d568c20d2c5ab43e404e51c599b66b86d87e6f4b76a971927cdef8099799a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SnapShop::Application.config.session_store :cookie_store, key: '_snap_shop_session'
